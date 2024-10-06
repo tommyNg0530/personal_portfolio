29f6f1fb45f8822cde66749c63160d5edfe8aca0
TommyNg <siuwang555@gmail.com>
2024-10-06 12:02:45 +0800

    Feat: Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
 import { BrowserRouter } from "react-router-dom";
-
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Chatbot } from "./components";
+import { Analytics } from "@vercel/analytics/react";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  Chatbot,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
           <About />
           <Tech />
-        </div>    
+        </div>
         <Experience />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Works />
           <Contact />
           <StarsCanvas />
         </div>
       </div>
+      <Analytics />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
